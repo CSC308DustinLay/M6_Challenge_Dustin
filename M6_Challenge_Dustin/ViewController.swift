8bda305f3c1e3f888368da5f58a864d7671a6e83
Dustin <redhotblood1010@gmail.com>
2026-02-26 12:18:13 -0500

    hehe

--- M6_Challenge_Dustin/ViewController.swift
+++ M6_Challenge_Dustin/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var modeControl: UISegmentedControl!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func resultButton(_ sender: Any)
+    {
+        
     }
-
-
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        resultLabel.text = " Try it Out!"
+        ageField.keyboardType = .numberPad
+    }
+    
+    //models
+    class Person
+    {
+        var name: String
+        var age: Int
+        
+        init(name: String, age: Int)
+        {
+            self.name = name
+            self.age = age
+        }
+        
+        convenience init()
+        {
+            self.init(name: "Anonymous", age: 0)
+        }
+        convenience init(name: String)
+        {
+            self.init(name: name, age: 0)
+        }
+        convenience init(name: String, using age: Int)
+        {
+            self.init(name: name, age: age)
+        }
+    }
+    
+    class validatedPerson
+    {
+        let name: String
+        let age: Int
+        
+        init?(name: String, age: Int)
+        {
+            guard !name.isEmpty, age > 0 else { return nil }
+            
+            self.name = name
+            self.age = age
+        }
+    }
 }
 
