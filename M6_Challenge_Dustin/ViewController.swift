f11a0455b80e583459e31780350cba07723cd950
Dustin <redhotblood1010@gmail.com>
2026-03-03 11:20:13 -0500

    hehe

--- M6_Challenge_Dustin/ViewController.swift
+++ M6_Challenge_Dustin/ViewController.swift
@@ -17,9 +17,69 @@ class ViewController: UIViewController {
     
     @IBAction func resultButton(_ sender: Any)
     {
+        let nameRaw = nameField.text ?? ""
+        let ageRaw = ageField.text ?? ""
         
+        switch modeControl.selectedSegmentIndex
+        {
+        case 0:
+            guard !nameRaw.isEmpty, !ageRaw.isEmpty, let age = Int(ageRaw) else
+            {
+                showAlert(title: "Error", message: "Ensure all fields are vaild")
+                return
+            }
+            let p = Person(name: nameRaw, age: age)
+            resultLabel.text = "Designated -> \(p.name), \(p.age)"
+            
+        case 1:
+            if nameRaw.isEmpty
+            {
+                let p = Person()
+                resultLabel.text = "Convenince -> \(p.name), \(p.age)"
+                return
+            }
+            
+            if ageRaw.isEmpty
+            {
+                let p = Person(name: nameRaw)
+                resultLabel.text = "Convenince2 -> \(p.name), \(p.age)"
+                return
+            }
+            
+            if let age = Int(ageRaw)
+            {
+                let p = Person(name: nameRaw, age: age)
+                resultLabel.text = "Convenince3 -> \(p.name), \(p.age)"
+            }
+            else
+            {
+                showAlert(title: "Invaild", message: "Age must be number")
+            }
+            
+        case 2:
+            guard !nameRaw.isEmpty, !ageRaw.isEmpty, let age = Int(ageRaw) else
+            {
+                showAlert(title: "Error", message: "Ensure all fields are vaild")
+                return
+            }
+            if let vp = validatedPerson(name: nameRaw, age: age)
+            {
+                resultLabel.text = "Failable -> \(vp.name), \(vp.age)"
+            }
+            
+            
+        default:
+            break
+        }
     }
     
+    func showAlert(title: String, message: String)
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
     
     override func viewDidLoad()
     {
